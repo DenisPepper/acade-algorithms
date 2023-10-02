7caef954f29f8be6bc84b68c7bebc3b5cef95b65
DenisPepper <thech@yandex.ru>
2023-10-02 17:25:08 +0300

    Добавит функцию, которая определяет факториал числа через цикл

--- factorial.ts
+++ factorial.ts
@@ -11,6 +11,27 @@ export const factorialByRecursion = (n: number): number => {
   return n * factorialByRecursion(n - 1);
 };
 
-
+//реализация через цикл
+export const factorialByLoop = (n: number): number => {
+    if (n < 0) {
+      throw new Error('n must be >= 0');
+    }
+  
+    if (n <= 1) {
+      return 1;
+    }
+  
+    let accumulator = n;
+    let counter = n - 1;
+    while (counter > 1) {
+      accumulator *= counter--;
+      /*
+      accumulator = accumulator * counter;
+      counter = counter - 1;
+      */
+    }
+  
+    return accumulator;
+  };
 
 
