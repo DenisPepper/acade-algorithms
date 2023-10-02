330db356ee8957c319184a22792659112e26718f
DenisPepper <thech@yandex.ru>
2023-10-02 18:43:25 +0300

    Добавит мемоизацию для функции вычисления ряда Фиббоначи

--- fibonacci-secuence.ts
+++ fibonacci-secuence.ts
@@ -1,11 +1,22 @@
+const store = new Map<number, number>();
+store.set(0, 0);
+store.set(1, 1);
+
 const fibSecuence = (n: number): number => {
   if (n < 0) {
     throw new Error('argument must be >= 0');
   }
 
-  if (n === 0) {
-    return 0;
+  if (store.has(n)) {
+    console.log('store');
+    return store.get(n) as number;
+  } else {
+    const result = fibSecuence(n - 1) + fibSecuence(n - 2);
+    console.log('evaluation');
+    store.set(n, result);
+    return result;
   }
-
-  return n <= 2 ? 1 : fibSecuence(n - 1) + fibSecuence(n - 2);
 };
+
+console.log(fibSecuence(8));
+console.log(fibSecuence(10));
