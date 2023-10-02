73fc44d9ba07731268c7f93d63ead171ed341478
DenisPepper <thech@yandex.ru>
2023-10-02 18:19:03 +0300

    Добавит мемоизацию для функции вычисления факториала

--- factorial.ts
+++ factorial.ts
@@ -1,3 +1,5 @@
+const store = new Map();
+
 //реализация через рекурсию
 export const factorialByRecursion = (n: number): number => {
   if (n < 0) {
@@ -8,30 +10,34 @@ export const factorialByRecursion = (n: number): number => {
     return 1;
   }
 
-  return n * factorialByRecursion(n - 1);
+  if (store.has(n)) {
+    return n;
+  } else {
+    const result = n * factorialByRecursion(n - 1);
+    store.set(n, result);
+    return result;
+  }
 };
 
 //реализация через цикл
 export const factorialByLoop = (n: number): number => {
-    if (n < 0) {
-      throw new Error('n must be >= 0');
-    }
-  
-    if (n <= 1) {
-      return 1;
-    }
-  
-    let accumulator = n;
-    let counter = n - 1;
-    while (counter > 1) {
-      accumulator *= counter--;
-      /*
+  if (n < 0) {
+    throw new Error('n must be >= 0');
+  }
+
+  if (n <= 1) {
+    return 1;
+  }
+
+  let accumulator = n;
+  let counter = n - 1;
+  while (counter > 1) {
+    accumulator *= counter--;
+    /*
       accumulator = accumulator * counter;
       counter = counter - 1;
       */
-    }
-  
-    return accumulator;
-  };
+  }
 
-
+  return accumulator;
+};
